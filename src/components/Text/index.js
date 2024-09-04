1b201b17861bf09aafea6e651dedf406a6f0ec77
ustinian-wang <wangjser@gmail.com>
2024-09-04 21:43:53 +0800

    feat: add text component

---
+++ src/components/Text/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledText from "./style.js";
+
+
+function Text({
+    children,
+    type = 'primary',
+    size = 'normal',
+    bold,
+    ...rest
+                }){
+    return (
+        <StyledText type={type} size={size} bold={bold} {...rest}>
+            {children}
+        </StyledText>
+    )
+}
+
+Text.propsType = {
+    children:  PropTypes.any,
+    type: PropTypes.oneOf(["primary", "secondary", "danger"]),
+    size: PropTypes.oneOf([
+        "xxsmall",
+        "xsmall",
+        "small",
+        "normal",
+        "medium",
+        "large",
+        "xlarge",
+        "xxlarge"
+    ]),
+    bold: PropTypes.bool
+}
+
+export default Text
